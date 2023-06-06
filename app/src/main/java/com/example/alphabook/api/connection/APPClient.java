5b7217d36ebde8e4eca3f52c1889eddf4ddf59d8
NIKHIL PRAJAPATI <nikboy010@gmail.com>
2023-06-06 15:53:03 +0530

    first commit

---
+++ app/src/main/java/com/example/alphabook/api/connection/APPClient.java
@@ -0,0 +1,27 @@
+package com.example.alphabook.api.connection;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class APPClient {
+    public static final String BASEURL = "https://alphabookproject.000webhostapp.com/";
+
+    public static Retrofit getctient() {
+
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASEURL)
+                .addConverterFactory(GsonConverterFactory.create(gson)).build();
+        return retrofit;
+    }
+
+
+}
